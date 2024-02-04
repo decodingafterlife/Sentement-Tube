dbec19885e140ecdd17fe33d34931cba23de783a
prajwalpkp2106 <padoleprajwal2106@gmail.com>
2024-02-04 11:52:26 +0530

    Home page and about us

--- app.py
+++ app.py
@@ -4,28 +4,47 @@ import numpy as np
 import youtube as yt
 from target_based_analysis_page import target
 from video_based_analysis_page import video
+from aboutus import about_us
+from PIL import Image 
 
 def home():
-    # Write You code here
+    # st.write("Understanding Audience Feedback Effectively")
+
+    st.markdown("<h3 style='text-align: center;'>Understanding Audience Feedback Effectively</h3>", unsafe_allow_html=True)
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.write(" ")
+    st.markdown("""<div style='border: 1px solid #e6e6e6; padding: 10px; border-radius: 5px;'><h2>Welcome to Sentiment-Tube</h2>
+Analyzing sentiments in comments provides valuable insights into audience reactions, preferences, and opinions, which can be beneficial for content creators, marketers, and platform administrators.</div>
+""", unsafe_allow_html=True)
+
     pass
 
 def main():
-    st.title("YouTube Video Sentiment Analysis")
+    im = Image.open('logo.png')#Title and App Icon
+    st.set_page_config(page_title="Sentiment-Tube", page_icon = im)
+    
+    hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden; }
+       footer {visibility: hidden;}
+       </style>
+       """
+    st.markdown(hide_default_format, unsafe_allow_html=True)#remove the Menu Button and Streamlit Icon
+    st.image('logo.png', width=700)
 
-    # Add a choice button to select between search for video or enter direct link
-    menu = ["Home", "Video Analysis", "Target Analysis"]
+    menu = ["Home", "Video Analysis", "Target Analysis", "About"]
     choice = st.sidebar.selectbox("Select Page", menu)
 
-    if (choice != "Video Analysis" and choice != "Target Analysis") or choice == "Home":
+    if choice == "Home":
         home()
-
-    if choice == "Video Analysis":
-        # Create a text input field for the user to enter the search query
+    elif choice == "Video Analysis":
         video()
-
     elif choice == "Target Analysis":
         target()
-        
-
+    elif choice == "About":
+        about_us()
+    
 if __name__ == "__main__":
     main()
